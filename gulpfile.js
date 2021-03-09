dfa559f769ae9c206df8bfd74e030689bd7db0b8
Ralph Steyer <ralph.steyer@rjs.de>
2021-03-09 09:52:45 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const {src, dest, series, parallel} = require('gulp');
+const del = require('del');
+
+const origin = 'src';
+const destination = 'build';
+
+async function clean(cb) {
+  await del(destination);
+  cb();
+}
+
+function html(cb) {
+  src(`${origin}/**/*.html`).pipe(dest(destination));
+  cb();
+}
+
+function css(cb) {
+  src(`${origin}/css/**/*.css`).pipe(dest(`${destination}/css`));
+  cb();
+}
+
+function js(cb) {
+  src([
+    `${origin}/js/lib/bootstrap.bundle.min.js`,
+    `${origin}/js/lib/fontawesome-all.min.js`,
+    `${origin}/js/lib/jquery.min.js`,
+    `${origin}/js/script.js`
+  ]).pipe(dest(`${destination}/js`));
+  cb();
+}
+
+exports.default = series(clean, parallel (html, css, js));
