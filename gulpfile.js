c0b6eaad902f8a8eb477207a9adb3bab7cc533f0
Ralph Steyer <ralph.steyer@rjs.de>
2021-03-09 18:41:46 +0100

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 const {src, dest, series, parallel, watch} = require('gulp');
 const del = require('del');
 const  browserSync = require("browser-sync").create();
-const sass = require('gulp-sass');
 const origin = 'src';
 const destination = 'build';
 
-sass.compiler = require('node-sass');
 
 async function clean(cb) {
   await del(destination);
@@ -20,7 +18,7 @@ function html(cb) {
 function css(cb) {
   src([`${origin}/css/animate.css`,`${origin}/css/bootstrap.min.css` ]).pipe(dest(`${destination}/css`));
 
-  src(`${origin}/css/style.css`).pipe(sass({outputStyle: 'compressed'})).pipe(dest(`${destination}/css`));
+  src(`${origin}/css/style.css`).pipe(dest(`${destination}/css`));
   cb();
 }
 
